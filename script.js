559a1f9886f5197a5c85aaf2a81258de1515e7e7
MGuibas <145477427+MGuibas@users.noreply.github.com>
2024-10-01 19:45:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const clientId = 'b6a4fbf9f4fc4646b8862fc9d157d071';
-const redirectUri = "https://mguibas.github.io/Spotify-songs-recomendation-page/";
+const redirectUri = 'http://127.0.0.1:5500/index.html';
 const scopes = 'user-top-read user-read-recently-played playlist-modify-private playlist-read-private';
 
 let accessToken;
